b6027f78f6475a8593f24ce72ffe4984638f96b4
betaviralhadas <beta_viralhadas@hotmail.com>
2024-07-20 11:03:22 +0100

    gallery page

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,5 +1,5 @@
 import styles from "./gallery.scss";
-import Layout from './layout'
+import Layout from '../layout'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import PhotosGallery from "../components/PhotosGallery"
